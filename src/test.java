5db4340227f7708835ca96638baa03da46aa2213
zht <zht@qq.com>
2020-08-08 22:36:36 +0800

    1234

--- src/test.java
+++ src/test.java
@@ -1,5 +1,7 @@
 public class test {
     public static void main(String[] args) {
+        public String id;
+        public String name;
         System.out.println("hello work");
         System.out.println("1111111");
         System.out.println("222222");
